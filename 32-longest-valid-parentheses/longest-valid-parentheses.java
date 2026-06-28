8d20d3ba8b4cb9cfd9bae61a048f6196fe2005a6
Akash Nath <akashnath.nath01@gmail.com>
2026-06-28 22:54:56 +0530

    Time: 5 ms (82.46%) | Memory: 46.2 MB (80.68%) - LeetSync

---
+++ 32-longest-valid-parentheses/longest-valid-parentheses.java
@@ -0,0 +1,28 @@
+import java.util.Stack;
+
+class Solution {
+    public int longestValidParentheses(String s) {
+        Stack<Integer> stack = new Stack<>();
+        stack.push(-1); // Base index
+
+        int maxLength = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(') {
+                stack.push(i);
+            } else {
+                stack.pop();
+
+                if (stack.isEmpty()) {
+                    // No matching '('
+                    stack.push(i);
+                } else {
+                    // Valid substring length
+                    maxLength = Math.max(maxLength, i - stack.peek());
+                }
+            }
+        }
+
+        return maxLength;
+    }
+}
